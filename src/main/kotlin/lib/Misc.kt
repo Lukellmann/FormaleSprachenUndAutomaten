75f300133513f7d716c9597227322a790cd37277
Lukellmann <lukellmann@gmail.com>
2021-11-09 06:34:34 +0100

    Canonical Collection representations, transitive closure

--- src/main/kotlin/lib/Misc.kt
+++ src/main/kotlin/lib/Misc.kt
@@ -1,3 +1,28 @@
 package lib
 
 fun Set<*>.setToString() = joinToString(separator = ", ", prefix = "{", postfix = "}")
+
+
+fun Set<Set<ContextFreeProduction>>.canonicalCollectionToString() =
+    joinToString(separator = ",\n  ", prefix = "{\n  ", postfix = "\n}") { each ->
+        each.joinToString(separator = ",\n    ", prefix = "{\n    ", postfix = "\n  }")
+    }
+
+fun Set<Set<ContextFreeProduction>>.canonicalCollectionToCompactString() =
+    joinToString(separator = ",\n  ", prefix = "{\n  ", postfix = "\n}") { it.setToString() }
+
+
+fun <T> transitiveClosure(start: Set<T>, step: (T) -> Iterable<T>): Set<T> {
+    if (start.isEmpty()) return start
+
+    val result = start.toMutableSet()
+    var new = start
+
+    while (true) {
+        new = new.flatMap { step(it) }.toSet() - result
+        if (new.isEmpty()) break
+        result.addAll(new)
+    }
+
+    return result
+}
