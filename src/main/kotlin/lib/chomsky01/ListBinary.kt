247093840fb0abbc9e22e02dee1028f1cd147085
Lukellmann <lukellmann@gmail.com>
2021-11-30 13:58:31 +0100

    Print Int.MAX_VALUE in binary

--- src/main/kotlin/lib/chomsky01/ListBinary.kt
+++ src/main/kotlin/lib/chomsky01/ListBinary.kt
@@ -3,9 +3,11 @@ package lib.chomsky01
 
 fun main() {
     (0..100).forEach { n -> println("$n: ${binaryNumber(n)}") }
+    println("${Int.MAX_VALUE}: ${binaryNumber(Int.MAX_VALUE)}")
 }
 
 
+// surjective (and injective -> bijective) recursive function
 private fun binaryNumber(n: Int): String {
     require(n >= 0)
 
--- src/main/kotlin/lib/chomsky01/ListPalindromes.kt
+++ src/main/kotlin/lib/chomsky01/ListPalindromes.kt
@@ -1,5 +1,3 @@
-@file:Suppress("SpellCheckingInspection")
-
 package lib.chomsky01
 
 
